4590ae9c27a2f1c374f391fe1c1fccbe2c313c6b
jason.yang <chyang590@gmail.com>
2017-07-01 15:19:53 +0800

    remove unused variable and decrease code indent

--- GOOS_SampleTests/Controllers/BugdgetControllerTests.cs
+++ GOOS_SampleTests/Controllers/BugdgetControllerTests.cs
@@ -19,11 +19,9 @@ namespace GOOS_SampleTests.Controllers
         public void AddTest_add_budget_successfully_should_invoke_budgetService_Create_one_time()
         {
             this._budgetController = new BudgetController(budgetServiceStub);
-            var model = new BudgetAddViewModel()
-                { Amount = 2000, Month = "2017-02" };
-            var result = this._budgetController.Add(model);
-            budgetServiceStub.Received()
-                .Create(Arg.Is<BudgetAddViewModel>(x => x.Amount == 2000 && x.Month == "2017-02"));
+            var model = new BudgetAddViewModel() { Amount = 2000, Month = "2017-02" };
+            this._budgetController.Add(model);
+            budgetServiceStub.Received().Create(Arg.Is<BudgetAddViewModel>(x => x.Amount == 2000 && x.Month == "2017-02"));
         }
     }
 }
